8be4b831d7b0f357b083a4b7d4d67d05fc7334ff
m <mserran2@swarthmore.edu>
2013-10-03 22:56:53 -0400

    - Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Songer::Application.config.secret_token = '21332ea8c1996849bdea5163884377865cef39e356ecd7405d30bfaf9add2283cad2070a1b33e545e8b7ee6845f819551ba3bc9fccc736d9072a6ae796b5489c'
