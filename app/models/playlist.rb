343ae33557ee386150ac731a32fef1d4048f6c9e
m <mserran2@swarthmore.edu>
2013-10-07 15:40:53 -0400

    - Add model validations for songs and playlists

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,5 +1,5 @@
 class Playlist < ActiveRecord::Base
   attr_accessible :name
-
+  validates_presence_of :name
   has_many :songs, :dependent => :destroy
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,4 +1,5 @@
 class Song < ActiveRecord::Base
   attr_accessible :album_150x150, :album_800x800, :album_title, :artist_name, :duration, :mnet_id, :title
   belongs_to :playlist
+  validates_presence_of :title, :duration, :album_title, :artist_name
 end
